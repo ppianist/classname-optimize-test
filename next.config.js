c1ffbefb5855e87a1d6dbbaad173273af87b693a
Jason <kwon_jason@yahoo.com>
2021-03-11 15:06:58 +1000

    add next-optimized-classnames package

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ const withPlugins = require("next-compose-plugins");
 
 const config = {};
 
-module.exports = withPlugins([], config);
+module.exports = withPlugins(
+    [
+        [require('next-optimized-classnames')]
+    ],
+    config,
+);
